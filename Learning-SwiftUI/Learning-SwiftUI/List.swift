b27e0b98bb3f05a3a49b0ffc5a3f8284e568259a
Vietdz123 <vietadu884772@gmail.com>
2023-10-13 08:47:18 -0700

    update readme

---
+++ Learning-SwiftUI/Learning-SwiftUI/List.swift
@@ -0,0 +1,8 @@
+//
+//  List.swift
+//  Learning-SwiftUI
+//
+//  Created by Tiến Việt Trịnh on 13/10/2023.
+//
+
+import Foundation
---
+++ Learning-SwiftUI/Learning-SwiftUI/TestFile.swift
@@ -0,0 +1,8 @@
+//
+//  TestFile.swift
+//  Learning-SwiftUI
+//
+//  Created by Tiến Việt Trịnh on 12/10/2023.
+//
+
+import Foundation
